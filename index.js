7b02e48f684aec2a4aebf70abb2a520865b95771
Vinit Singh <79987261+VinitSingh13@users.noreply.github.com>
2022-03-24 11:57:31 +0530

    cloned website files

--- index.js
+++ index.js
@@ -44,12 +44,15 @@ document.querySelector(".front").addEventListener("click",function(){
     let a = document.querySelector(".bg-Img");
     if(window.getComputedStyle(a.children[0]).visibility==="visible"){
       url = window.getComputedStyle(a.children[0]).backgroundImage.split("PracticeProject6/").pop().slice(0,-2);
+      console.log("url");
     }
     else if(window.getComputedStyle(a.children[1]).visibility==="visible"){
       url = window.getComputedStyle(a.children[1]).backgroundImage.split("PracticeProject6/").pop().slice(0,-2);
+      console.log("url");
     }
     else if(window.getComputedStyle(a.children[2]).visibility==="visible"){
       url = window.getComputedStyle(a.children[2]).backgroundImage.split("PracticeProject6/").pop().slice(0,-2);
+      console.log("url");
     }
     count=1;
   }
